cd285425e36db88101767d651c823dadb5ca73d4
DawnGlow <ferrater1013@gmail.com>
2024-03-22 19:48:28 +0900

    add TetrisMain
    메인 Level 추가
    InProgress : 미완성 기능

---
+++ src/InProgress/Setting.java
@@ -0,0 +1,19 @@
+package InProgress;
+
+import javax.swing.*;
+
+public class Setting extends JFrame {
+    public JPanel panel;
+
+
+    public Setting() {
+        panel = new JPanel();
+
+        setVisible(true);
+        setSize(400, 800);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+
+
+}
--- src/main/Tetris.java
+++ src/main/Tetris.java
@@ -1,12 +1,45 @@
 package main;
 
+import InProgress.ImportSettings;
+import env.TetrisMain;
 import component.Board;
 
 public class Tetris {
+
+	private static ImportSettings settings;
+
+	private static TetrisMain main;
+
+	private static Board tetrisGame;
+
+
+	private static int ResolutionSizeX;
+
+	private static int ResolutionSizeY;
+
+	private static void initialize() {
+		settings = new ImportSettings();
+		ResolutionSizeX = Integer.parseInt(settings.getSetting("ResolutionSizeX"));
+		ResolutionSizeY = Integer.parseInt(settings.getSetting("ResolutionSizeY"));
+	}
+
+	public static void startGame() {
+		main.setVisible(false);
+		tetrisGame = new Board();
+		tetrisGame.setSize(ResolutionSizeX, ResolutionSizeY);
+		tetrisGame.setVisible(true);
+	}
+
+	public static void LevelMain() {
+		tetrisGame.setVisible(false);
+		main.setVisible(true);
+	}
+
 //we
 	public static void main(String[] args) {
-		Board main = new Board();
-		main.setSize(400, 800);
+		initialize();
+		main = new TetrisMain(ResolutionSizeX,ResolutionSizeY);
 		main.setVisible(true);
 	}
 }
+
